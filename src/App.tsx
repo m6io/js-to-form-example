7b6945c07a543b3f8a5abf3411ef7fac2fffa9e2
m6io <m5dk4r@gmail.com>
2024-06-22 15:33:56 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { Form } from "js-to-form"
+import { JSONSchema7 } from "json-schema"
+import schema from "./schema.json"
+
+const App = () => {
+  const formSchema: JSONSchema7 = schema as JSONSchema7
+
+  return (
+    <div>
+      <h1>{formSchema.title || "Untitled Form"}</h1>
+      <p>{formSchema.description || null}</p>
+      <hr />
+      <Form
+        schema={formSchema as JSONSchema7}
+        onSubmit={(data) => console.log(data)}
+        onError={(errors) => console.error(errors)}
+      />
+    </div>
+  )
+}
+
+export default App
